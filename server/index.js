376716f570659a6e52e8828d84f8f630f5fe7c47
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-07-26 07:32:42 +0100

    Update port value
    changing port from 3000 to 5000

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const PORT = process.env.PORT || 3000;
 
 const app = next({ dev });
 const handler = app.getRequestHandler();
@@ -24,7 +23,7 @@ app
       return handler(req, res);
     });
 
-    server.listen(PORT, (err) => {
+    server.listen(5000, (err) => {
       if (err) throw err;
       console.log(`Ready on ${PORT}...`);
     });
